3868fe17702c31d4854ec6b4a2f7446b7a277f29
rfree <rfree@gmx.us>
2015-04-16 10:55:06 +0200

    New version (used on suchdig for long)

--- index.php
+++ index.php
@@ -13,21 +13,22 @@
 */
 
 	// configure options:
-	$urlbase = "https://p.l/"; // <--- CONFIGURE ME! configurable - url address of the pastebin
+	$urlbase = ""; // <--- configurable - url address of the pastebin, default ""
 	$dbg=0; // debug the code? 1=developer version, 0=production
 	$mode='I'; // mode: I=integer-like counter with generating URLs "aaa" "aab" "aac". 
 		// CL=checkSums long based urls, same text has identical URL (long and crypto-secured against forgery/collision) (TODO)
 
 	$data_subdir = "p/"; // configurable - subdirectory for data. INSTALLATION - read above
 	$counter_subdir = "c/"; // configurable - subdirectory for counter. INSTALLATION - read above
-	$data_len_max = 256*1024; // configurable - maximum size of text (in bytes) at one past
-	$data_len_maxfile = 4*1024*1024; // configurable - maximum size of file upload (in bytes) at one paste
-	$totall_limit = 100 * 1024 * 1024; // bytes - maximum size of sum of ALL pastes as counted in the counter file
+	$data_len_max = 512*1024; // configurable - maximum size of text (in bytes) at one past
+	$data_len_maxfile = 128*1024*1024; // configurable - maximum size of file upload (in bytes) at one paste
+	$totall_limit = 100 * 1024 * 1024 * 1024; // bytes - maximum size of sum of ALL pastes as counted in the counter file
 	// there are also hard limits/sanity checks look for [hardlimit] in code
 
 	// privacy/metadata choices:
+	$metadata = 0; // 0=no 1=save public .ini files
 	$metadata_include_text = 0; // in the -info metadata file should we append the entire text? for text only.
-	$metadata_include_sha = 1;  // in the -info metadata file should we append checksum of the entire text? for text only.
+	$metadata_include_sha = 0;  // in the -info metadata file should we append checksum of the entire text? for text only.
 	// TODO support sha sum of files pasted?
 	$metadata_sha_salt = ""; // salt to prepend for _sha, "" for none (then equals sha1sum of file)
 
@@ -44,10 +45,29 @@
 	$counter_random_inc = 10; // increment counter by random up to this number for (a bit of extra) privacy. 1 or more.
 	$counter_random_zero = 5000; // random start of counter, to hide exact number of pastes, for privacy.
 
-	// change if you updated this script:
+	$terms_notice='Files might be deleted by admins after a while (days, weeks?). We do not obey requests for removal, so think before you send!';
 	$author_homepage="https://github.com/rfree/trivialpaste/"; // project homepage, shown e.g. in the bugreport and footer 
+	$email1='i.am.lawyer.or.other.ahole -on- gmail.com';  $email2='rfree -in- i2pmail.org OR &lt;rfree&gt; on IRC.oftc.net';
+	$terms="Test beta deployment of open public pastebin with no oversight from server or domains owners. ".
+	"This service is granted with 0 guarantees, we can delete any content or block users as we see fit for no reason. ".
+	"DMCA notices or other bullshit should be sent to: $email1, we resere 52 weeks of time to reply. ".
+	"Contact for non-legal issues: <b>$email2</b> (by sending legal data or spam to that address you agree to pay 500$ [per each started 1000 characters] fee for processing :).";
+
+// ------ END OF CONFIGURATION DATA. BELOW IS CONSTANT SOURCE CODE PART ------
 ?>
 <?php // general functions e.g. escape html
+	function return_bytes ($size_str) {
+		switch (substr ($size_str, -1)) {
+        		case 'K': case 'k': return (int)$size_str * 1024;
+        		case 'M': case 'm': return (int)$size_str * 1024*1024;
+        		case 'G': case 'g': return (int)$size_str * 1024*1024*1024;
+        		case 'T': case 't': return (int)$size_str * 1024*1024*1024*1024;
+        		default: return $size_str;
+   		}
+	}
+	$a = array('upload_max_filesize','post_max_size'); foreach ($a as $k) { $limit_tab["ini_get($k)"] = return_bytes( ini_get($k) ); }
+	$limit_tab['data_len_maxfile']=$data_len_maxfile; $limit_tab['totall_limit']=$totall_limit; asort($limit_tab);
+
 	function _txt($t) { return htmlspecialchars($t); }
 	function __txt($t) { echo htmlspecialchars($t); }
 	function get_author_homepage() { global $author_homepage; return $author_homepage; }
@@ -57,6 +77,20 @@
 		for($r = ""; $n >= 0; $n = intval($n / 26) - 1) $r = chr($n%26 + ord('a')) . $r;
 		return $r;
 	}
+	function bytesToSize($Bs, $precision = 2)
+	{  
+		$kiloB = 1024;   $megaB = $kiloB * 1024;   $gigaB = $megaB * 1024;    $teraB = $gigaB * 1024;
+		if (($Bs < $kiloB)) { return $Bs . ' B'; } 
+		elseif (($Bs >= $kiloB) && ($Bs < $megaB)) { return round($Bs / $kiloB, $precision) . ' KiB'; } 
+		elseif (($Bs >= $megaB) && ($Bs < $gigaB)) { return round($Bs / $megaB, $precision) . ' MiB'; } 
+		elseif (($Bs >= $gigaB) && ($Bs < $teraB)) { return round($Bs / $gigaB, $precision) . ' GiB'; } 
+		elseif ($Bs >= $teraB) { return round($Bs / $teraB, $precision) . ' TiB'; };
+		return '??? B';
+	}
+	function nice_filename($s) {
+		$s=preg_replace('/[^0-9a-zA-Z()\[\]_-]/', '_', $s); // kill non standard chars
+		return preg_replace('/^\.+/','X',$s); // last, kill any heading dots (heading slash was killed already)
+	} // echo nice_filename('../../foo.bar FOO 01234 2+2=4 ,.;ąćż↑↓ fa-bender-r(2).ex[freedom].moo.deb/inside/dir/a/b'); // test
 ?>
 <?php
 	// === paste number and totall size atomic file-based counter function ===
@@ -135,6 +169,9 @@
 		h1 { font-size: 125%; }
 		.err { color: red; font-weight: bold; }
 		textarea { background: #def; color: #000; }
+		i.foot { color: #999; font-size: 11px; }
+		.terms_notice { color: #777; } 
+		.pasted { border:4px solid black; padding: 4px; margin: 8px; }  .pasted:hover { background-color: #ee7; }
 	</style>
 </head>
 
@@ -188,9 +225,7 @@ Trivial Paste at <a href="<?php echo _txt($urlbase); ?>"><?php echo _txt($urlbas
 		$data_any = isset($_POST["text"]);
 		if ($data_any) {
 			$data = $_POST["text"] ;
-			$data_ok = true;
-			$data_len = strlen($data);
-			$data_real = $data_any && $data_len;
+			$data_ok = true;  $data_len = strlen($data);  $data_real = $data_any && $data_len;
 		}
 		$data_too_big = $data_len > $data_len_max;
 
@@ -219,11 +254,15 @@ Trivial Paste at <a href="<?php echo _txt($urlbase); ?>"><?php echo _txt($urlbas
 				$end = substr($end,$seglen);
 			}
 			mkdir($data_subdir.$pre,0700,true); // make dir for the segments of pastename. 123/456/ for 123456xyz
+			$orgpre=''; // orginal-filename based prepend
+			if (! $data_is_text) { 
+				if ($_POST['preorgname']) $end = $end . '-' . nice_filename(pathinfo($data_file_name, PATHINFO_BASENAME)) ;
+			}
 			$pastename = $pre.$end; // 123/456/xyz
 			$filename = $data_subdir.$pastename; // p/123/456/xyz
 
 			if (isset($_POST['prename'])) {
-				$prename = urlencode( $_POST['prename'] ); // TODO better sanitize!
+				$prename = nice_filename( $_POST['prename'] );
 				if (strlen($prename) > 0) {
 					if (strlen($prename) > 100) die('Too long prename.');
 					$filename .= '-' . $prename;
@@ -231,11 +270,11 @@ Trivial Paste at <a href="<?php echo _txt($urlbase); ?>"><?php echo _txt($urlbas
 			}
 
 			$file_ext = 'txt';
-			if (! $data_is_text) {
-				$file_ext = pathinfo($data_file_name, PATHINFO_EXTENSION);
+			if (! $data_is_text) { 
+				$file_ext = nice_filename(pathinfo($data_file_name, PATHINFO_EXTENSION));
 			} 
-			$filename1 = $filename.'.'.$file_ext;
-			$filename2 = $filename.'.ini'; // for metadata
+			$filename1 = ($filename.'.'.$file_ext);
+			$filename2 = ($filename.'.ini'); // for metadata (if used)
 			$url1 = $urlbase . _txt($filename1);
 			$url2 = $urlbase . _txt($filename2);
 			$info="";
@@ -264,36 +303,47 @@ Trivial Paste at <a href="<?php echo _txt($urlbase); ?>"><?php echo _txt($urlbas
 				if (! move_uploaded_file($data_file_tmp, $filename1)) die('Error writting the file (datafile move)');
 			}
 
-			if (! @file_put_contents($filename2, $info)) die('Error writting the file (info)');
-			echo '<p>Pasted: <b><a href="'._txt($url1).'">'._txt($url1).'</a></b> '
-				.' [+<a href="'._txt($url2).'">info</a>]'
-				.'</p>';
+			if ($metadata) {  if (! @file_put_contents($filename2, $info)) die('Error writting the file (info)');  }
+			echo '<div class="pasted"><b><u>Pasted:</u></b> <b><a href="'._txt($url1).'">'._txt($url1).'</a></b></div>';
+			echo '<p><a href="/">Goto: paste again</a></p>';
+			if ($metadata) echo ' [+<a href="'._txt($url2).'">info</a>]';
+			echo '</p>';
 			$msg_yet=true;
 		} // the paste.
 	} // probably will paste
 
 	if (!$msg_yet) { // no message (error or the success url) written yet? then write this prompt:
-		echo '<p>Paste the text in box below, it will be <b>published</b> on this page on generated shortened URL.</p>';
+		echo '<p>Paste text <u>or</u> file. Will be published on short URL.</p>';
 	}
 ?>
 
-<div>
+<span class="terms_notice"><?php echo $terms_notice; ?></span>
+
 	<form method="POST" enctype="multipart/form-data" >
 		<div>
-		<input type="file" name="thefile" size="25" /> 
-		<input type="submit" value="Send" />
-		- upload file, or enter text:<br/>
-		<input type="text" name="prename" value="" /> prepend name?<br/>
-		<textarea name="text" cols="80" rows="30"><?php
-			if (isset($data)) {	echo _txt($data);	}
-		?></textarea>
-		</div>
-		<input type="submit" value="Send" />
+	Custom title?
+	
+	<label><input type="checkbox" name="preorgname" <?php if ($_POST['preorgname']) echo 'checked="checked"'; ?> id="preorgname" />from-orginal </label> 
+	+ prepend <input type="text" name="prename" value="<?php if ($_POST['prename']) echo _txt($_POST['prename']); ?>" /> (+extension)<br/>
+	<input type="file" name="thefile" size="25" /> - upload a file 
+	(max <?php $l=$limit_tab; echo '<abbr title="limited by '.key($l).'">'.bytesToSize(current($l)).'</abbr>';?> ), 
+	<u>or</u> enter text 
+	(max <?php $l=$limit_tab; $l['data_len_max']=$data_len_max; asort($l); echo '<abbr title="limited by '.key($l).'">'.bytesToSize(current($l)).'</abbr>';?>) 
+	below:<br />
+	<input type="submit" value="Send" /><br/>
+	<textarea name="text" cols="120" rows="30"><?php
+		if (isset($data)) {	echo _txt($data);	}
+	?></textarea>
+	</div>
+	<input type="submit" value="Send" />
 	</form>
-</div>
 
 <hr/>
-<a href="<?php echo _txt($author_homepage);?>">Trivial Paste</a> 0.93 by <a href="https://github.com/rfree">rfree</a>.
+<a href="<?php echo _txt($author_homepage);?>">Trivial Paste</a> 0.95 by <a href="https://github.com/rfree">rfree</a>.
+<hr/> 
+Donate? 
+<a href="noblecoin://9qkw7KUvv69Dajo7NYoVWkZyxqaPPLyBKX"><b>Noblecoin</b></a> <sup><a href="http://www.noblemovement.com/">?</a></sup> 
+<br/><br/><i class="foot"><?php echo $terms; ?></i>
 
 </body>
 </html>
